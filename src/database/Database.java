09fa7e0c2ad7a6d3d3624088f1ad05b9b95ca849
Edward <Edouardos@csd.auth.gr>
2021-11-23 21:55:57 +0200

    Removed DatabaseType from Database,added Serialization
    Signed-off-by: Edward <Edouardos@csd.auth.gr>

--- src/database/Database.java
+++ src/database/Database.java
@@ -1,13 +1,49 @@
 package database;
 
-public abstract class Database {
+import java.io.*;
 
-    protected final DatabaseType type;
+public abstract class Database implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 0;
 
-    public DatabaseType getType() {
-        return type;
+
+    protected String filename;
+
+    public Database(String filename) {
+        this.filename = filename;
     }
 
+
+    public void write() {
+        try {
+            File temp = new File(this.filename);
+            //noinspection ResultOfMethodCallIgnored
+            temp.createNewFile(); // create file if not present
+            FileOutputStream fileOut = new FileOutputStream(this.filename);
+            ObjectOutputStream objectOut = new ObjectOutputStream(fileOut);
+            objectOut.writeObject(this);
+            objectOut.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    //check
+    public Object read() {
+        Object obj = null;
+        try {
+            File temp = new File(this.filename);
+            //noinspection ResultOfMethodCallIgnored
+            temp.createNewFile(); // create file if not present
+            FileInputStream fileIn = new FileInputStream(this.filename);
+            ObjectInputStream objectIn = new ObjectInputStream(fileIn);
+            obj = objectIn.readObject();
+            objectIn.close();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return obj;
+    }
 
     public String getFilename() {
         return filename;
@@ -17,15 +53,5 @@ public abstract class Database {
         this.filename = filename;
     }
 
-    protected String filename;
-
-    public Database(DatabaseType type, String filename) {
-        this.filename = filename;
-        this.type = type;
-    }
-
-
-    public abstract void write();
-
-    public abstract void read();
 }
+
