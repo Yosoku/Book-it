571d66bf99bcb6059d4c1630639917e4882abe15
Edward <Edouardos@csd.auth.gr>
2021-11-23 21:25:53 +0200

    Fixed Serialazible not implemented
    Signed-off-by: Edward <Edouardos@csd.auth.gr>

---
+++ src/database/ReservationCustomer.java
@@ -0,0 +1,10 @@
+package database;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+public class ReservationCustomer implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 0;
+
+}
