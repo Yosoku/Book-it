285741aa9dff3f6be240f36f4183f39a7ba190c9
Edward <Edouardos@csd.auth.gr>
2021-11-23 22:16:52 +0200

    Added Serialization to class Added new member variable Added Filename for read/write Added new queries
    Signed-off-by: Edward <Edouardos@csd.auth.gr>

--- src/database/BrokerAccommodations.java
+++ src/database/BrokerAccommodations.java
@@ -3,49 +3,81 @@ package database;
 import accommodations.Accommodation;
 import users.Broker;
 
+import java.io.Serial;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-public class BrokerAccommodations extends Database {
+public class BrokerAccommodations extends Database implements Serializable {
+    private int totalAccommodations;
+    @Serial
+    private static final long serialVersionUID = 0;
     private final HashMap<Broker, ArrayList<Accommodation>> brokerProperties;
 
     public BrokerAccommodations() {
-        super(DatabaseType.BROKER_ACCOMMODATIONS, "");
+        super("src/config/brokerAccommodations.ser");
         brokerProperties = new HashMap<Broker, ArrayList<Accommodation>>();
+        totalAccommodations = 0;
     }
 
 
-    public void insertAccommodation(Accommodation accommodation, Broker broker) {
+    public void insertAccommodation(Broker broker, Accommodation accommodation) {
         if (broker == null || accommodation == null)
             return;
         ArrayList<Accommodation> accommodationList = brokerProperties.get(broker);
         if (accommodationList == null)
             accommodationList = new ArrayList<>();
+        accommodation.setID(getNextID());
         accommodationList.add(accommodation);
         brokerProperties.put(broker, accommodationList);
     }
 
-    public List<Accommodation> selectAccommodation(Broker broker) {
+    public ArrayList<Accommodation> selectAllAccommodationsFromBroker(Broker broker) {
         if (brokerProperties.containsKey(broker))
             return brokerProperties.get(broker);
         return null;
     }
 
 
-    public void dropAccommodation(Accommodation accommodation, Broker broker) {
+    public void dropAccommodation(Broker broker, Accommodation accommodation) {
         if (broker == null || accommodation == null)
             return;
         brokerProperties.get(broker).remove(accommodation);
     }
 
-    @Override
-    public void write() {
-
+    public void updateAccommodation(Accommodation newAccommodation, Accommodation oldAccommodation, Broker broker) {
+        if (broker == null || oldAccommodation == null || newAccommodation == null)
+            return;
+        brokerProperties.get(broker).remove(oldAccommodation);
+        brokerProperties.get(broker).add(newAccommodation);
     }
 
-    @Override
-    public void read() {
 
+    public ArrayList<Accommodation> selectAllAccommodations() {
+        ArrayList<Accommodation> temp = new ArrayList<Accommodation>();
+        for (List<Accommodation> list : brokerProperties.values()) {
+            temp.addAll(list);
+        }
+        return temp;
+    }
+
+    private int getNextID() {
+        return ++totalAccommodations;
+    }
+
+    public Accommodation selectAccommodationByID(int id) {
+        for (ArrayList<Accommodation> accommodations : brokerProperties.values())
+            for (Accommodation accommodation : accommodations)
+                if (id == accommodation.getID())
+                    return accommodation;
+        return null;
+    }
+
+    public Accommodation selectAccommodationByID(Broker broker, int id) {
+        for (Accommodation accommodation : brokerProperties.get(broker))
+            if (id == accommodation.getID())
+                return accommodation;
+        return null;
     }
 }
