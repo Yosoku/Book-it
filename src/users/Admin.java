27fc3e1cfe2b839a9b3bccc5fa1cc386eea84c89
Edward <Edouardos@csd.auth.gr>
2021-11-23 21:51:15 +0200

    Added Javadoc
    Signed-off-by: Edward <Edouardos@csd.auth.gr>

--- src/users/Admin.java
+++ src/users/Admin.java
@@ -1,8 +1,20 @@
 package users;
 
+import auth.Credentials;
+
+/**
+ * <p>
+ * A Admin class representing a Admin in an application. The Admin class inherits from the abstract Class User
+ * resulting in inheritance of all the fields (Credentials, Name, Age,Email,Gender, Phone number) and its getter/setter
+ * methods for each field. It also provides a toString() override useful for printing a Broker class to a stream
+ * </p>
+ *
+ * @author Edward Koulakidis
+ * @see User
+ */
 public class Admin extends User {
-    public Admin(String name, int age, String username, String email, Gender gender, String phone, String password) {
-        super(name, age, username, email, gender, phone, password);
+    public Admin(Credentials credentials, String name, int age, String email, Gender gender, String phone) {
+        super(credentials, name, age, email, gender, phone, Privilege.ADMIN);
     }
 }
 
