3ed97703366ce87c468d3b66102d998eff463908
Edward <Edouardos@csd.auth.gr>
2021-11-24 15:00:30 +0200

    Removed comments
    Signed-off-by: Edward <Edouardos@csd.auth.gr>

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 
+import application.Application;
+
 import java.security.NoSuchAlgorithmException;
 
 
@@ -6,8 +8,5 @@ public class Main {
 
     public static void main(String[] args) throws NoSuchAlgorithmException {
         Application app = new Application();
-
-
-        //752a43e39a08b5bcaedf9302c82e78c8a8c9514755dd5de28fadf11aa7be2d8af8415c4acff7a403bf835c86fc1d16b3237d72a4dcde55443fb4657e28fc9d10
     }
 }
