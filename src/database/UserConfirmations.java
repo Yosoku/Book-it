c1f7a2eb08bee7b91a59a29080edabac7d277053
Edward <Edouardos@csd.auth.gr>
2021-11-24 12:36:41 +0200

    Readded UserConfirmations
    Signed-off-by: Edward <Edouardos@csd.auth.gr>

---
+++ src/database/UserConfirmations.java
@@ -0,0 +1,62 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+package database;
+
+import users.User;
+
+import java.io.Serial;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class UserConfirmations extends Database {
+    private HashMap<User, Boolean> userConfirmed = new HashMap<User, Boolean>();
+    @Serial
+    private static final long serialVersionUID = 0L;
+
+    public UserConfirmations() {
+        super("src/config/userConfirmation.ser");
+    }
+
+    public void insertUserConfirmation(User user) {
+        if (user != null) {
+            this.userConfirmed.put(user, false);
+        }
+    }
+
+    public boolean selectUserConfirmation(User user) {
+        if (user == null)
+            return false;
+        return userConfirmed.get(user);
+    }
+
+    public void dropUserConfirmation(User user) {
+        if (user != null) {
+            this.userConfirmed.remove(user);
+        }
+    }
+
+    public void updateUserConfirmation(User user, boolean confirmed) {
+        if (user != null) {
+            this.userConfirmed.put(user, confirmed);
+        }
+    }
+
+    public List<User> selectAllUsersWhereConfirmedIs(boolean confirmed) {
+        List<User> temp = new ArrayList<User>();
+        for (User user : this.userConfirmed.keySet()) {
+            if (this.userConfirmed.get(user) == confirmed) {
+                temp.add(user);
+            }
+        }
+
+        return temp;
+    }
+
+    public ArrayList<User> selectAllUsers() {
+        return new ArrayList<>(this.userConfirmed.keySet());
+    }
+}
