396f9911cfbd561311fbbd0880a456a60d559fdd
Edward <Edouardos@csd.auth.gr>
2021-11-28 21:57:58 +0200

    Added javadoc and code clean up
    Signed-off-by: Edward <Edouardos@csd.auth.gr>

--- src/database/UserConfirmations.java
+++ src/database/UserConfirmations.java
@@ -12,39 +12,93 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+
+/**
+ * <p>
+ * The UserConfirmations is a class intended to handle information about accounts in an application. It is implemented by a
+ * Hashtable holding Users->Boolean. It basically holds information about a Users confirmation status</p>
+ * <p>
+ * The UserConfirmations inherits from Database inheriting read/write methods and implementing Serializable interface. It also
+ * provides basic queries like SELECT,INSERT and DROP. It is also important to note that <b>null</b> values are not stored
+ * in this class</p>
+ *
+ * @author Edward Koulakidis
+ * @see java.io.Serializable
+ * @see Database
+ */
+
 public class UserConfirmations extends Database {
-    private HashMap<User, Boolean> userConfirmed = new HashMap<User, Boolean>();
+    private HashMap<User, Boolean> userConfirmed;
     @Serial
     private static final long serialVersionUID = 0L;
 
+    /**
+     * An initializer constructor used to initialize the map and call the Base class with the specified filename to store
+     * the map
+     */
     public UserConfirmations() {
         super("src/config/userConfirmation.ser");
+        userConfirmed = new HashMap<User, Boolean>();
     }
 
+    /**
+     * A query for inserting new Users in the Database. If any of the parameters is <b>null</b> the method will exit
+     * without changing the Hashmap. Runs in O(1) time
+     *
+     * @param user The new User to insert as confirmed=false
+     */
     public void insertUserConfirmation(User user) {
         if (user == null)
             return;
-        userConfirmed.putIfAbsent(user, false);
+        userConfirmed.putIfAbsent(user, false); // Users are initially inserted as false
     }
 
+    /**
+     * A query for selecting Users in the Database. If any of the parameters is <b>null</b> the method will exit
+     * without changing the Hashmap. Runs in O(1) time
+     *
+     * @param user The new User to select
+     * @return Returns the boolean status of the User's confirmation status. Returns <b>ALWAYS</b> false if the user is null
+     */
     public boolean selectUserConfirmation(User user) {
         if (user == null)
             return false;
         return userConfirmed.get(user);
     }
 
+    /**
+     * A query for dropping(deleting) Users from the Database. If any of the parameters is <b>null</b> the method will exit
+     * without changing the Hashmap. Runs in O(1) time
+     *
+     * @param user The User to delete from the map
+     */
     public void dropUserConfirmation(User user) {
         if (user == null)
             return;
         this.userConfirmed.remove(user);
     }
 
+    /**
+     * A query for updating current Users in the Database. If any of the parameters is <b>null</b> the method will exit
+     * without changing the Hashmap. Runs in O(1) time
+     *
+     * @param user      The User to update from the map
+     * @param confirmed The new confirmation status
+     */
     public void updateUserConfirmation(User user, boolean confirmed) {
         if (user == null)
             return;
         this.userConfirmed.put(user, confirmed);
     }
 
+    /**
+     * A query for selecting <b>ALL</b> Users from the Database whose confirmation status==confirmed. Runs in O(n) time
+     * Example: <code>selectAllUsersWhereConfirmedIs(true)</code> returns all the Users who have confirmation status
+     * as true
+     *
+     * @param confirmed The boolean status to query
+     * @return A List with all the Users with confirmation status==confirmed
+     */
     public List<User> selectAllUsersWhereConfirmedIs(boolean confirmed) {
         List<User> temp = new ArrayList<User>();
         for (User user : this.userConfirmed.keySet()) {
@@ -55,7 +109,16 @@ public class UserConfirmations extends Database {
         return temp;
     }
 
+    /**
+     * A method useful for selecting all the Users in the Database. Runs in O(1) time
+     *
+     * @return Returns an ArrayList with all the Users registered in the map regardless of confirmation status
+     */
     public ArrayList<User> selectAllUsers() {
-        return new ArrayList<>(this.userConfirmed.keySet());
+        for (User user : userConfirmed.keySet()) {
+            System.out.println(user.toString());
+            System.out.println(userConfirmed.get(user));
+        }
+        return new ArrayList<>(userConfirmed.keySet());
     }
 }
