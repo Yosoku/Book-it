4d5668e551e7472de1977f4a2c24dd6e9d836c62
Edward <Edouardos@csd.auth.gr>
2021-11-28 21:56:23 +0200

    Added custom serialVersion and finished javadoc
    Signed-off-by: Edward <Edouardos@csd.auth.gr>

--- src/auth/Credentials.java
+++ src/auth/Credentials.java
@@ -3,10 +3,27 @@ package auth;
 import java.io.Serial;
 import java.io.Serializable;
 
+/**
+ * <p>
+ * A Credentials record representing credentials in an authentication process. It provides 2 final members (username/password)
+ * and 2 getter methods for accessing said fields. It also provides a toString() override useful for printing the class
+ * in a stream or hashing the contents of the class. The record is very similar to PasswordAuthentication
+ * </p>
+ *
+ * @author Edward Koulakidis
+ * @see java.net.PasswordAuthentication
+ */
 public record Credentials(String username, String password) implements Serializable {
     @Serial
     private static final long serialVersionUID = 0;
 
+    /**
+     * A toString override useful for hashing a User's credentials in a Database
+     *
+     * @return The username concatenated with the password
+     * @see database.Database
+     * @see Encryption
+     */
     @Override
     public String toString() {
         return username + password;
