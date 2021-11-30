9f0ae7d1d1e42dc3fb12c4d785fdddc31b8f4d50
Edward <Edouardos@csd.auth.gr>
2021-11-30 11:43:20 +0200

    Added a User field
    Signed-off-by: Edward <Edouardos@csd.auth.gr>

--- src/UI/CustomerUI.java
+++ src/UI/CustomerUI.java
@@ -2,13 +2,21 @@ package UI;
 
 import accommodations.Accommodation;
 import communication.Review;
+import users.User;
 
-public class CustomerUI extends UI{
+public class CustomerUI extends UI {
+    private User user;
+
+    public CustomerUI(User currentUser) {
+        super();
+        user = currentUser;
+    }
+
     @Override
     public void show() {
         System.out.println("Welcome back User,how would you like to proceed?\nView reservations(viewres)\n" +
                 "View reviews(viewrev)\nSearch Accommodations(search)");
-        request = getInput("Enter viewres/viewrev/search","(search|viewrev|viewres)");
+        request = getInput("Enter viewres/viewrev/search", "(search|viewrev|viewres)");
 
     }
 
@@ -16,9 +24,8 @@ public class CustomerUI extends UI{
     public Review addReview(Accommodation accommodation){
         int stars = Integer.parseInt(getInput("How many stars does the accommodation get?","[1-5]"));
         String description = getInput("Write a small description for your review ","");
-        return new Review(stars,profanityFilter(description));
+        return new Review(stars, profanityFilter(description), user, accommodation);
     }
 
-    public void searchAccommodations() {
-    }
+
 }
