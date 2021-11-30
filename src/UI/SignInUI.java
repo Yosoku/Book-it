5bc1740eee349f38defab9210ccbc718b9b3f13b
Edward <Edouardos@csd.auth.gr>
2021-11-30 11:49:21 +0200

    Fixed the regex to match usernames ending with digits
    Signed-off-by: Edward <Edouardos@csd.auth.gr>

--- src/UI/SignInUI.java
+++ src/UI/SignInUI.java
@@ -3,10 +3,10 @@ package UI;
 public class SignInUI extends UI {
 
 
-    //TODO fix regex doesnt support passwords ending with numbers
+
     public void show() {
         request = getInput("Enter your username and password separated by white space",
-                "\\b[a-zA-Z]+\\s[a-zA-Z]+\\b");
+                "\\w*[a-zA-Z]\\w*+\\s\\w*[a-zA-Z]\\w*+\\b");
     }
 
 }
