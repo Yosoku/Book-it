2a63802de5f4af65eacadc125f64f68c93ef8d70
Edward <Edouardos@csd.auth.gr>
2021-11-30 11:40:00 +0200

    Added User and Accommodation to Review record and made it Serializable
    Signed-off-by: Edward <Edouardos@csd.auth.gr>

--- src/communication/Review.java
+++ src/communication/Review.java
@@ -1,19 +1,33 @@
 package communication;
 
 
-public record Review(int stars, String description) {
-    public int getStars() {
-        return stars;
-    }
+import accommodations.Accommodation;
+import users.User;
 
-    public String getDescription() {
-        return description;
-    }
+import java.io.Serial;
+import java.io.Serializable;
 
-    public Review(int stars, String description) {
+public record Review(int stars, String description, User user,
+                     Accommodation accommodation) implements Serializable {
+    public Review(int stars, String description, User user, Accommodation accommodation) {
         this.description = description;
+        this.user = user;
+        this.accommodation = accommodation;
         if (stars < 0)
             this.stars = 1;
         else this.stars = Math.min(stars, 5);
     }
+
+    @Override
+    public String toString() {
+        return "Review{" +
+                "stars=" + stars +
+                ", description='" + description + '\'' +
+                ", user=" + user +
+                ", accommodation=" + accommodation +
+                '}';
+    }
+
+    @Serial
+    private static final long serialVersionUID = 0;
 }
