133e53099f8fff44f130be5d1fcf8784d2a2f938
Edward <Edouardos@csd.auth.gr>
2021-12-11 14:30:31 +0200

    A class for handling Broker requests
    Signed-off-by: Edward <Edouardos@csd.auth.gr>

--- src/requests/BrokerRequests.java
+++ src/requests/BrokerRequests.java
@@ -5,9 +5,13 @@ import UI.UI;
 import UI.UIMessage;
 import accommodations.Accommodation;
 import application.DatabaseAPI;
+import communication.Message;
 import users.Broker;
+import users.User;
 
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 
 public class BrokerRequests implements Handler {
     private Broker broker;
@@ -27,14 +31,21 @@ public class BrokerRequests implements Handler {
                 case "view" -> {
                     HashSet<Accommodation> accommodationList =
                             DatabaseAPI.getBrokerAccommodationsDatabase().selectAllAccommodationsFromBroker(broker);
-                    for (Accommodation accommodation : accommodationList) {
-                        System.out.println(accommodation.toString());
+                    if (accommodationList == null) {
+                        System.out.println("You have not added any accommodations yet.");
+                    } else {
+                        for (Accommodation accommodation : accommodationList) {
+                            System.out.println(accommodation.toString());
+                        }
                     }
 
                 }
                 case "add" -> {
                     Accommodation accommodation = ui.addAccommodation();
-                    DatabaseAPI.getBrokerAccommodationsDatabase().insertAccommodation(broker, accommodation);
+                    if (accommodation == null)
+                        System.out.println("Invalid Accommodation");
+                    else
+                        DatabaseAPI.getBrokerAccommodationsDatabase().insertAccommodation(broker, accommodation);
                 }
                 case "edit" -> {
                     int id = Integer.parseInt(ui.getInput("Enter the id of the Accommodation you wish to edit", "^[1-9][0-9]*$"));
@@ -52,8 +63,19 @@ public class BrokerRequests implements Handler {
                         UI.LOG(UIMessage.ENTRY_NOT_FOUND);
                     } else {
                         DatabaseAPI.getBrokerAccommodationsDatabase().dropAccommodation(broker, accommodation);
-                        //  accommodationReviewsDatabase.dropAllReviewsFromAccommodation(accommodation);
-                        //add removeall reservations
+                        List<User> affectedUsers = new LinkedList<User>();
+                        DatabaseAPI.getReviewsDatabase().selectReviewsByAccommodation(accommodation).forEach(review -> {
+                            affectedUsers.add(review.user());
+                        });
+                        DatabaseAPI.getReservationsDatabase().selectReservationsByAccommodation(accommodation).forEach(reservation -> {
+                            affectedUsers.add(reservation.user());
+                        });
+                        affectedUsers.forEach(user -> DatabaseAPI.getUserMessagesDatabase().insertMessageToUser(user,
+                                new Message(broker, user, "Deleted Accommodation", "I regret to inform you that" +
+                                        "the Accommodation" + accommodation.toString() + "was removed so any reservations/reviews" +
+                                        "you had associated with it will be removed as well.")));
+                        DatabaseAPI.getReservationsDatabase().dropAllReservationsByAccommodation(accommodation);
+                        DatabaseAPI.getReviewsDatabase().dropAllReviewsByAccommodation(accommodation);
                         UI.LOG(UIMessage.ENTRY_DELETED);
                     }
 
