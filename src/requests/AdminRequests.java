dbe40c23b6dd1c7bab96b5a462e06b05b0dff4dc
Edward <Edouardos@csd.auth.gr>
2021-12-11 14:31:07 +0200

    A class for handling Admin requests
    Signed-off-by: Edward <Edouardos@csd.auth.gr>

--- src/requests/AdminRequests.java
+++ src/requests/AdminRequests.java
@@ -6,6 +6,8 @@ import communication.Message;
 import users.Admin;
 import users.User;
 
+import java.util.List;
+
 public class AdminRequests implements Handler {
     private AdminUI ui;
     private Admin admin;
@@ -23,12 +25,25 @@ public class AdminRequests implements Handler {
             ui.show();
             switch (ui.getRequest()) {
                 case "confirm" -> {
-                    String confirmationMessage = "Hello reply to this email to confirm your account";
-                    for (User user : DatabaseAPI.getUserConfirmationsDatabase().selectAllUsersWhereConfirmedIs(false)) {
-                        System.out.println("Send confirmation message to user " + user.toString() + "\ny/n");
-                        char ans = ui.getInput("y/n", "y|Y|N|n").toLowerCase().charAt(0);
-                        if (ans == 'y')
-                            DatabaseAPI.getUserMessagesDatabase().insertMessageToUser(user, new Message(admin, user, confirmationMessage));
+                    String confirmationMessage = "Hello read this email to confirm your account";
+                    List<User> unconfirmed = DatabaseAPI.getUserConfirmationsDatabase().selectAllUsersWhereConfirmedIs(false);
+                    System.out.printf("There are currently %d unconfirmed users. Would you like to confirmed them all?\n"
+                            , unconfirmed.size());
+                    char ans = ui.getInput("y/n", "y|Y|n|N").charAt(0);
+                    if (ans == 'y') {
+                        System.out.println("Sending confirmation message to all Unconfirmed Users...");
+                        unconfirmed.forEach(user -> DatabaseAPI.getUserMessagesDatabase().
+                                insertMessageToUser(user, new Message(admin, user, Message.auth, confirmationMessage)));
+                    } else {
+                        for (User user : unconfirmed) {
+                            System.out.printf("Send confirmation message to user? q to exit\n %s", user.toString());
+                            ans = ui.getInput("y/n/q", "q|Q|y|Y|N|n").toLowerCase().charAt(0);
+                            if (ans == 'y')
+                                DatabaseAPI.getUserMessagesDatabase().insertMessageToUser
+                                        (user, new Message(admin, user, Message.auth, confirmationMessage));
+                            if (ans == 'q')
+                                break;
+                        }
                     }
                 }
                 case "stats" -> {
