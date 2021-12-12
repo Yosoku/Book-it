735fb697c56e3abd6795376cbec69a75348a1ff9
Edward <Edouardos@csd.auth.gr>
2021-12-12 12:52:33 +0200

    -sleep added for better visual experience
    Signed-off-by: Edward <Edouardos@csd.auth.gr>

--- src/UI/WelcomeUI.java
+++ src/UI/WelcomeUI.java
@@ -1,42 +1,22 @@
 package UI;
 
-import java.util.concurrent.TimeUnit;
+import application.Application;
 
 public class WelcomeUI extends UI {
     @Override
     public void show() {
         int readDelay = 2;
         System.out.println("Welcome to the Alpha version of Booking Clone");
-        try {
-            TimeUnit.SECONDS.sleep(readDelay);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        Application.sleepFor(readDelay);
         System.out.println("The application currently features over 100.000 users.\nPlease make sure to read the README" +
                 " as it provides useful information and a variety of admin accounts");
-        try {
-            TimeUnit.SECONDS.sleep(readDelay + 2);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        Application.sleepFor(readDelay + 2);
         System.out.println("Please note that the features provided to the Users are limited,as they are a work in progress");
-        try {
-            TimeUnit.SECONDS.sleep(readDelay + 1);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        Application.sleepFor(readDelay + 1);
         System.out.println("Booking clone has been thoroughly tested,if you find any bugs please contact the developer\n" +
                 "> edouardos@csd.auth.gr");
-        try {
-            TimeUnit.SECONDS.sleep(readDelay + 3);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        Application.sleepFor(readDelay + 3);
         System.out.println("The application will now launch. Please enjoy");
-        try {
-            TimeUnit.SECONDS.sleep(readDelay + 4);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        Application.sleepFor(readDelay + 4);
     }
 }
