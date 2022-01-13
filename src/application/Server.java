0a2bd629b4bb47bd08ce07342fcd9114cb01265a
Edward Koulakidis <Edouardos@csd.auth.gr>
2022-01-13 22:40:10 +0200

    Added static requests in Server
    Signed-off-by: Edward Koulakidis <Edouardos@csd.auth.gr>

--- src/application/Server.java
+++ src/application/Server.java
@@ -1,10 +1,9 @@
 package application;
 
-import UI.SignInUI;
-import UI.SignUpUI;
+import GUI.ConnectUI.SignInPanel;
+import GUI.ConnectUI.SignUpPanel;
 import UI.UI;
 import UI.UIMessage;
-import auth.Credentials;
 import requests.AdminRequests;
 import requests.BrokerRequests;
 import requests.CustomerRequests;
@@ -22,48 +21,56 @@ import users.User;
  * @author Edward Koulakidis
  */
 public class Server {
+    private static String currentRequest = "";
     public boolean quit;
     private User currentUser = null;
 
+    public static String getCurrentRequest() {
+        return currentRequest;
+    }
+
+    public static void sendRequest(String newRequest) {
+        Application.getInstance().notifyThread();
+        currentRequest = newRequest;
+    }
+
     /**
      * Method for establishing a connection to the Server
-     * @param request The request to handle,can be either 'signin','signup' or 'signout'
+     *
      * @return true if a connection has been established,false otherwise
      */
-    public boolean handleConnectionRequests(String request) {
-        switch (request) {
+    public boolean handleConnectionRequests() {
+        switch (currentRequest) {
             case "signin" -> {
-                SignInUI signin = new SignInUI();
-                signin.show();
-                String[] request1 = signin.getRequest().split("\\s");
-                Credentials credentials = new Credentials(request1[0], request1[1]);
-                currentUser = DatabaseAPI.credentialsUserDatabase.selectUser(credentials);
+                currentUser = SignInPanel.user;
                 if (currentUser == null) {
                     UI.LOG(UIMessage.SIGN_IN_FAILED);
+                    Application.getInstance().notifyThread();
                     return false;
                 } else {
                     UI.LOG(UIMessage.SIGN_IN_SUCCESS);
+                    Application.getInstance().notifyThread();
                     return true;
                 }
             }
             case "signup" -> {
-                SignUpUI signup = new SignUpUI();
-                signup.show();
-                currentUser = signup.getNewUser();
+                currentUser = SignUpPanel.user;
                 if (DatabaseAPI.credentialsUserDatabase.selectUser(currentUser.getCredentials()) != null) { // duplicate credentials
                     UI.LOG(UIMessage.SIGN_UP_FAILED);
+                    Application.getInstance().notifyThread();
                     return false;
                 } else {
                     UI.LOG(UIMessage.SIGN_UP_SUCCESS);
                     DatabaseAPI.credentialsUserDatabase.insertUser(currentUser.getCredentials(), currentUser);
                     DatabaseAPI.userConfirmationsDatabase.insertUserConfirmation(currentUser);
+                    Application.getInstance().notifyThread();
                     return true;
                 }
             }
         }
+
         return false;
     }
-
 
     /**
      * The method responsible for pointing requests to their appropriate Handler e.g. Customer Handler,Broker Handler etc
