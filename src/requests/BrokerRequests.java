60749c915f9d58364b8b41167db2afb789ae5c7d
Edward Koulakidis <Edouardos@csd.auth.gr>
2022-01-13 22:18:32 +0200

    Added case files in request handling method to handle GUI components
    Signed-off-by: Edward Koulakidis <Edouardos@csd.auth.gr>

--- src/requests/BrokerRequests.java
+++ src/requests/BrokerRequests.java
@@ -1,20 +1,15 @@
 package requests;
 
+import GUI.AppWindow;
+import GUI.Inbox.InboxView;
+import GUI.ProfileView;
+import GUI.usersgui.BrokerHomePage;
 import UI.BrokerUI;
-import UI.InboxUI;
-import UI.UI;
-import UI.UIMessage;
-import accommodations.Accommodation;
-import accommodations.Reservation;
-import application.Application;
 import application.DatabaseAPI;
-import communication.Message;
+import application.Server;
 import users.Broker;
-import users.User;
 
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
+import javax.swing.*;
 
 /**
  * A class representing a Request Handler for Broker Requests. It holds information about the current Broker making
@@ -33,8 +28,8 @@ public class BrokerRequests implements Handler {
      * @param broker The broker making requests
      */
     public BrokerRequests(Broker broker) {
-        ui = new BrokerUI(broker);
         this.broker = broker;
+        AppWindow.getInstance().changePanel(new BrokerHomePage(broker));
     }
 
     /**
@@ -43,71 +38,38 @@ public class BrokerRequests implements Handler {
      */
     @Override
     public void handleRequests() {
-        boolean quit = false;
-        while (!quit) {
-            ui.show();
-            switch (ui.getRequest()) {
-                case "view" -> {
-                    HashSet<Accommodation> accommodationList =
-                            DatabaseAPI.brokerAccommodationsDatabase.selectAllAccommodationsFromBroker(broker);
-                    if (accommodationList.isEmpty())
-                        System.out.println("You have not added any accommodations yet.");
-                    else
-                        accommodationList.forEach(System.out::println);
-                    Application.sleepFor(2);
-                }
-                case "add" -> {
-                    Accommodation accommodation = ui.addAccommodation();
-                    if (accommodation == null)
-                        System.out.println("Invalid Accommodation");
-                    else
-                        DatabaseAPI.brokerAccommodationsDatabase.insertAccommodation(broker, accommodation);
-                    System.out.println("A new Accommodation has been added succesfully");
-                }
-                case "edit" -> {
-                    int id = Integer.parseInt(ui.getInput("Enter the id of the Accommodation you wish to edit", "^[1-9][0-9]*$"));
-                    Accommodation accommodation = DatabaseAPI.brokerAccommodationsDatabase.selectAccommodationByID(broker, id);
-                    if (accommodation == null) {
-                        UI.LOG(UIMessage.ENTRY_NOT_FOUND);
-                    } else {
-                        ui.editAccommodation(accommodation);
-                    }
-                }
-                case "delete" -> {
-                    int id = Integer.parseInt(ui.getInput("Enter the ID of the Accommodation you wish to delete", "^[1-9][0-9]*$"));
-                    Accommodation accommodation = DatabaseAPI.brokerAccommodationsDatabase.selectAccommodationByID(broker, id);
-                    if (accommodation == null) {
-                        UI.LOG(UIMessage.ENTRY_NOT_FOUND);
-                    } else {
-                        DatabaseAPI.brokerAccommodationsDatabase.dropAccommodation(broker, accommodation);
-                        List<User> affectedUsers = new LinkedList<User>();
-                        DatabaseAPI.reviewsDatabase.selectReviewsByAccommodation(accommodation).forEach(review -> affectedUsers.add(review.user()));
-                        DatabaseAPI.reservationDatabase.selectReservationsByAccommodation(accommodation).forEach(reservation -> affectedUsers.add(reservation.user()));
-                        affectedUsers.forEach(user -> DatabaseAPI.userMessagesDatabase.insertMessageToUser(user,
-                                new Message(broker, user, "Deleted Accommodation", "I regret to inform you that" +
-                                        "the Accommodation" + accommodation + "was removed so any reservations/reviews" +
-                                        "you had associated with it will be removed as well.")));
-                        DatabaseAPI.reservationDatabase.dropAllReservationsByAccommodation(accommodation);
-                        DatabaseAPI.reviewsDatabase.dropAllReviewsByAccommodation(accommodation);
-                        UI.LOG(UIMessage.ENTRY_DELETED);
-                    }
 
-                }
-                case "viewres" -> {
-                    HashSet<Reservation> reservations = DatabaseAPI.reservationDatabase.selectAllReservationsByBroker(broker);
-                    if (reservations.isEmpty())
-                        System.out.println("Your Accommodations have no active Reservations");
-                    else
-                        reservations.forEach(System.out::println);
-                    Application.sleepFor(2);
-                }
-                case "inbox" -> {
-                    InboxUI inbox = new InboxUI(broker);
-                    inbox.show();
-                }
 
-                case "signout" -> quit = true;
+        switch (Server.getCurrentRequest()) {
+            case "view" -> {
+
+
             }
+            case "add" -> {
+
+            }
+            case "edit" -> {
+
+            }
+            case "delete" -> {
+
+
+            }
+            case "profile" -> {
+                SwingUtilities.invokeLater(() -> {
+                    JFrame profileView = new ProfileView(broker);
+                    profileView.setVisible(true);
+                });
+
+            }
+            case "inbox" -> {
+                SwingUtilities.invokeLater(() -> {
+                    InboxView inboxView = new InboxView(DatabaseAPI.userMessagesDatabase.selectMessageFromUser(broker));
+                    inboxView.setVisible(true);
+                });
+
+            }
+            case "signout" -> SwingUtilities.invokeLater(() -> AppWindow.getInstance().signOut());
         }
     }
 }
