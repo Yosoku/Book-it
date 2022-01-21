53f5f6883e32db82d4e3a6c1cf110ad51699367c
Edward Koulakidis <Edouardos@csd.auth.gr>
2022-01-21 16:23:16 +0200

    Added enter key functionality
    Signed-off-by: Edward Koulakidis <Edouardos@csd.auth.gr>

--- src/frontend/ConnectUI/SignInPanel.java
+++ src/frontend/ConnectUI/SignInPanel.java
@@ -12,6 +12,8 @@ import backend.users.User;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 /**
  * @author Edward
@@ -43,36 +45,56 @@ public class SignInPanel extends JPanel {
         setMinimumSize(new Dimension(300, 300));
         setMaximumSize(new Dimension(300, 300));
         setPreferredSize(new Dimension(300, 300));
-        setBorder(new javax.swing.border.CompoundBorder(new javax.swing.border.TitledBorder(new javax.swing
-                .border.EmptyBorder(0, 0, 0, 0), "JF\u006frmD\u0065sig\u006eer \u0045val\u0075ati\u006fn", javax.swing.border.TitledBorder
-                .CENTER, javax.swing.border.TitledBorder.BOTTOM, new java.awt.Font("Dia\u006cog", java.
-                awt.Font.BOLD, 12), java.awt.Color.red), getBorder()))
-        ;
+        setBackground(new Color(0, 21, 36));
+        setBorder(new javax.swing.border.CompoundBorder(new javax.swing.border.TitledBorder(new javax.swing.border.EmptyBorder
+                (0, 0, 0, 0), "", javax.swing.border.TitledBorder.CENTER, javax.swing.border
+                .TitledBorder.BOTTOM, new java.awt.Font("", java.awt.Font.BOLD, 12), java.awt
+                .Color.red), getBorder()));
         addPropertyChangeListener(e -> {
-            if ("\u0062ord\u0065r".equals(e.getPropertyName())) throw new RuntimeException();
-        })
-        ;
+            if ("\u0062order".equals(e.getPropertyName())) throw new RuntimeException()
+                    ;
+        });
         setLayout(null);
 
         //---- usernameLabel ----
         usernameLabel.setText("Username");
+        usernameLabel.setBackground(new Color(221, 199, 160));
+        usernameLabel.setFont(usernameLabel.getFont().deriveFont(usernameLabel.getFont().getSize() + 5f));
+        usernameLabel.setForeground(new Color(221, 199, 160));
         add(usernameLabel);
-        usernameLabel.setBounds(0, 10, 85, usernameLabel.getPreferredSize().height);
+        usernameLabel.setBounds(5, 5, 110, usernameLabel.getPreferredSize().height);
+
+        //---- usernameField ----
+        usernameField.setBackground(new Color(0, 21, 36));
+        usernameField.setFont(usernameField.getFont().deriveFont(usernameField.getFont().getSize() + 3f));
+        usernameField.setForeground(new Color(255, 236, 209));
+        usernameField.setCaretColor(new Color(255, 236, 209));
         add(usernameField);
-        usernameField.setBounds(0, 25, 300, 30);
+        usernameField.setBounds(0, 30, 300, 30);
 
         //---- passwordLabel ----
         passwordLabel.setText("Password");
+        passwordLabel.setForeground(new Color(221, 199, 160));
+        passwordLabel.setFont(passwordLabel.getFont().deriveFont(passwordLabel.getFont().getSize() + 5f));
         add(passwordLabel);
-        passwordLabel.setBounds(0, 65, 100, passwordLabel.getPreferredSize().height);
+        passwordLabel.setBounds(5, 70, 100, passwordLabel.getPreferredSize().height);
+
+        //---- passwordField ----
+        passwordField.setBackground(new Color(0, 21, 36));
+        passwordField.setFont(passwordField.getFont().deriveFont(passwordField.getFont().getSize() + 3f));
+        passwordField.setForeground(new Color(255, 236, 209));
+        passwordField.setCaretColor(new Color(255, 236, 209));
         add(passwordField);
-        passwordField.setBounds(0, 80, 300, 30);
+        passwordField.setBounds(0, 90, 300, 30);
 
         //---- enterButton ----
-        enterButton.setText("Sign In");
+        enterButton.setIcon(new ImageIcon(getClass().getResource("/ConnectPanel/signin.png")));
+        enterButton.setBorder(null);
+        enterButton.setBorderPainted(false);
+        enterButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
         enterButton.addActionListener(this::enter);
         add(enterButton);
-        enterButton.setBounds(new Rectangle(new Point(220, 155), enterButton.getPreferredSize()));
+        enterButton.setBounds(190, 140, 100, 40);
 
         {
             // compute preferred size
@@ -89,6 +111,22 @@ public class SignInPanel extends JPanel {
             setPreferredSize(preferredSize);
         }
         // JFormDesigner - End of component initialization  //GEN-END:initComponents
+
+        passwordField.addKeyListener(new KeyListener() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ENTER)
+                    enter(null);
+            }
+
+            @Override
+            public void keyTyped(KeyEvent e) {
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+            }
+        });
     }
 
     // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
