fc583ecfc5915be75a92f6d87fa46be311b00d23
Edward Koulakidis <Edouardos@csd.auth.gr>
2022-01-21 19:02:11 +0200

    tests
    Signed-off-by: Edward Koulakidis <Edouardos@csd.auth.gr>

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,15 @@ public class Main {
 
 
     public static void main(String[] args) {
-       Application.getInstance().run();
+        //Bugs found and not fixed in Windows (Linux distros have been tested and run without crashes/bugs)
+        // Windows JOptionPanes show on bottom of all the windows and can't be alttabbed (nice os)
+        // Try to move the window before confirming actions
+
+
+        // Den teliwsa ta test,den eixa allo xrono,thelw ligi epieikeia mias kai hmoun monos =)
+        // Oti exception/crash vreite anoikste ena issue sto github h steilte mou ena email na to diorthwsw gia na kanw to
+        // repo public archive afou to kanw refactor ligo akoma. Steile mou ena email otan mporw na ksanakanw commit xwris
+        // na epireastei to deadline
+        Application.getInstance().run();
     }
 }
